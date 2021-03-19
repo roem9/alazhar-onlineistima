71b5567a9b27f5f857a91434cb731ae41d43dbd1
roem9 <muhammadrum04@gmail.com>
2021-03-19 13:05:01 +0700

    goo

--- application/views/pages/soal/soal-toafl.php
+++ application/views/pages/soal/soal-toafl.php
@@ -182,7 +182,7 @@
                                                                     <div class="text-right">
                                                                         <label for="soal_istima<?= $i.$k?>" class="mr-2" id="container-content"><?= $data?></label>
                                                                     </div>
-                                                                    <input type="radio" class="soal_istima" id="soal_istima<?= $i.$k?>" name="soal_istima[<?= $i?>]" value="<?= $data?>" <?php if($k == 0){echo "required";}?> checked>
+                                                                    <input type="radio" class="soal_istima" id="soal_istima<?= $i.$k?>" name="soal_istima[<?= $i?>]" value="<?= $data?>" <?php if($k == 0){echo "required";}?>>
                                                                 </div>
                                                             <?php endforeach;?>
                                                         </div>
@@ -236,7 +236,7 @@
                                                                     <div class="text-right">
                                                                         <label for="soal_tarakib<?= $i.$k?>" class="mr-2" id="container-content"><?= $data?></label>
                                                                     </div>
-                                                                    <input type="radio" class="soal_tarakib" id="soal_tarakib<?= $i.$k?>" name="soal_tarakib[<?= $i?>]" value="<?= $data?>" <?php if($k == 0){echo "required";}?> checked>
+                                                                    <input type="radio" class="soal_tarakib" id="soal_tarakib<?= $i.$k?>" name="soal_tarakib[<?= $i?>]" value="<?= $data?>" <?php if($k == 0){echo "required";}?>>
                                                                 </div>
                                                             <?php endforeach;?>
                                                         </div>
@@ -371,7 +371,7 @@
                                                                     <div class="text-right">
                                                                         <label for="soal_qiroah<?= $i.$k?>" class="mr-2" id="container-content"><?= $data?></label>
                                                                     </div>
-                                                                    <input type="radio" class="soal_qiroah" id="soal_qiroah<?= $i.$k?>" name="soal_qiroah[<?= $i?>]" value="<?= $data?>" <?php if($k == 0){echo "required";}?> checked>
+                                                                    <input type="radio" class="soal_qiroah" id="soal_qiroah<?= $i.$k?>" name="soal_qiroah[<?= $i?>]" value="<?= $data?>" <?php if($k == 0){echo "required";}?>>
                                                                 </div>
                                                             <?php endforeach;?>
                                                         </div>
